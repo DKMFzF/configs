e1dd7897b194b23cbf40b64992795b488eea8d29
Doroshev Kirill <150276575+DKMFzF@users.noreply.github.com>
2025-07-24 15:40:07 +0300

    feat: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,128 @@
 module.exports = {
-  root: true,
   env: {
-    browser: true, 
-    es2021: true, 
-    node: true,  
+    browser: true,
+    es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true, 
+      jsx: true,
     },
-    ecmaVersion: 12, 
-    sourceType: 'module', 
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   plugins: [
     'react',
-    'prettier',
+    '@typescript-eslint',
   ],
   rules: {
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
-    'semi': ['error', 'always'],
-    'max-len': ['error', { 'code': 80, 'ignoreUrls': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true, 'ignoreRegExpLiterals': true }],
-    'camelcase': ['error', { 'properties': 'never' }],
-    'new-cap': ['error', { 'newIsCap': true, 'capIsNew': false }],
-    'no-unused-vars': 'warn', 
-    'react/react-in-jsx-scope': 'off', 
-    'react/prop-types': 'off', 
-    'prettier/prettier': ['error', {
-      'singleQuote': true,
-      'semi': true,
-      'tabWidth': 2,
-      'printWidth': 80,
-      'trailingComma': 'es5' 
+    // Базовые правила
+    'no-var': 'error',
+    'no-unused-vars': 'error',
+    eqeqeq: ['error', 'always'],
+    'default-case': 'error',
+    
+    // Правила для console.log
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    
+    // Стиль именования
+    'camelcase': ['error', { 
+      properties: 'always',
+      ignoreDestructuring: false,
+      ignoreImports: false,
+      ignoreGlobals: false,
     }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'variable',
+        modifiers: ['const'],
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: false, // Запрещаем префикс I для интерфейсов
+        },
+      },
+    ],
+
+    // Форматирование
+    'indent': ['error', 2],
+    'max-len': ['error', { 
+      code: 120,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true,
+    }],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+    'quotes': ['error', 'single', { 
+      avoidEscape: true,
+      allowTemplateLiterals: true,
+    }],
+
+    // React правила
+    'react/jsx-pascal-case': ['error', {
+      allowAllCaps: false,
+      ignore: [],
+    }],
+    'react/react-in-jsx-scope': 'off',
+
+    // Комментарии
+    'spaced-comment': ['error', 'always', {
+      markers: ['/'],
+      exceptions: ['-', '+'],
+    }],
+    'capitalized-comments': ['off'], // Отключаем проверку заглавных букв
+    'multiline-comment-style': ['error', 'separate-lines'],
   },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx', '**/*.test.js', '**/*.test.jsx'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'max-len': ['error', { code: 150 }], // Разрешаем более длинные строки в тестах
+      },
+    },
+    {
+      files: ['**/*.debug.ts', '**/*.debug.tsx', '**/*.debug.js', '**/*.debug.jsx'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
   settings: {
     react: {
-      version: 'detect', 
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}, // Для корректной работы с TypeScript путями
     },
   },
 };
-
